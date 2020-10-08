f614feaf344536e573d01b7bb1bf328f9a37aa32
smeeeetha <72543921+smeeeetha@users.noreply.github.com>
2020-10-08 13:24:29 +0530

    Create print-the-elements-of-a-linked-list-in-reverse.cpp
    Uses the concept of recursion.

---
+++ Contest2/Orange/print-the-elements-of-a-linked-list-in-reverse.cpp
@@ -0,0 +1,19 @@
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+void reversePrint(SinglyLinkedListNode* head) 
+{ 
+    if(head==NULL)
+        return;
+    else
+       {
+        reversePrint(head->next); 
+        cout<<head->data<<endl;
+       }
+}
